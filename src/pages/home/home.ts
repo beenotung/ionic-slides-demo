6ff196bf6dd4aa3d32427264e03e70d18d8258a0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2018-08-25 17:16:18 +0800

    init comit
    added demo for ion-slides

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +7,20 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  mode = 'demo';
+  xs = [];
+  code = '<ion-slides pager [slidesPerView]="3" *ngSwitchCase="\'demo\'">\n' +
+    '  <ion-slide *ngFor="let x of xs">\n' +
+    '    <img src="assets/imgs/logo.png">\n' +
+    '    <p>The {{ x + 1 }} / {{ xs.length }} image</p>\n' +
+    '  </ion-slide>\n' +
+    '</ion-slides>';
 
+  constructor(public navCtrl: NavController) {
+    this.xs = [];
+    for (let i = 0; i < 20; i++) {
+      this.xs.push(i)
+    }
   }
 
 }
